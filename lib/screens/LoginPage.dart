687423f2d6f9c6af83f464e584a764627875311f
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-02-21 23:14:28 +0530

    overflow fix

--- lib/screens/LoginPage.dart
+++ lib/screens/LoginPage.dart
@@ -26,114 +26,116 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              GradientScaffold(
-                height: 230.0, 
-                width: contextWidth(context),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              const Text("Welcome to",
-                style: TextStyle(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                GradientScaffold(
+                  height: 230.0, 
+                  width: contextWidth(context),
                 ),
-              ),
-              const Text("Social Clip",
-                style: TextStyle(
-                  fontSize: 34.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red
+                const SizedBox(
+                  height: 15.0,
                 ),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 200.0,
-                  width: 300.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: const Color(0xffFFE9E9),
+                const Text("Welcome to",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextFormField(
-                              controller: _emailController,
-                              keyboardType: TextInputType.emailAddress,
-                              // ignore: prefer_const_constructors
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                              ),
-                          ),
-                        ),
-                         const SizedBox(height: 10),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextFormField(
-                            controller: _passwordController,
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              labelText: 'Password',
+                ),
+                const Text("Social Clip",
+                  style: TextStyle(
+                    fontSize: 34.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red
+                  ),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 200.0,
+                    width: 300.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: const Color(0xffFFE9E9),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                                controller: _emailController,
+                                keyboardType: TextInputType.emailAddress,
+                                // ignore: prefer_const_constructors
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                ),
                             ),
                           ),
-                        ),                 
-                      ],
+                           const SizedBox(height: 10),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              controller: _passwordController,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                              ),
+                            ),
+                          ),                 
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              AppButton(
-                  height: 63.0,
-                  width: 216.0,
-                  color: Colors.red,
-                  textdata: "Log in",
-                  fontSize: 23.0,
-                  fontWeight: FontWeight.bold,
-                  radius: 20.0,
-                  onTap: (){
-                    Navigator.pushNamed(context, NavigationPage.route);
-                  },
+                const SizedBox(
+                  height: 15.0,
                 ),
-                const SizedBox(height: 15.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Dont have an account? ",
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () => Navigator.pushNamed(context, SignUpPage.route),
-                      child: const Text(
-                        "Sign up here",
+                AppButton(
+                    height: 63.0,
+                    width: 216.0,
+                    color: Colors.red,
+                    textdata: "Log in",
+                    fontSize: 23.0,
+                    fontWeight: FontWeight.bold,
+                    radius: 20.0,
+                    onTap: (){
+                      Navigator.pushNamed(context, NavigationPage.route);
+                    },
+                  ),
+                  const SizedBox(height: 15.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Dont have an account? ",
                         style: TextStyle(
                           fontSize: 14.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red
+                          fontWeight: FontWeight.bold
                         ),
                       ),
-                    )
-                  ],
-                )
-            ],
+                      InkWell(
+                        onTap: () => Navigator.pushNamed(context, SignUpPage.route),
+                        child: const Text(
+                          "Sign up here",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+              ],
+            ),
           ),
         ),
       )
--- lib/screens/SignUpPage.dart
+++ lib/screens/SignUpPage.dart
@@ -22,106 +22,108 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              GradientScaffold(
-                height: 230.0, 
-                width: contextWidth(context),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 283.0,
-                  width: 300.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xffFFE9E9),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: TextFormField(
-                              controller: _nameController,
-                              decoration: InputDecoration(
-                                labelText: 'Full Name',
-                              ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: TextFormField(
-                            controller: _emailController,
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: const InputDecoration(
-                              labelText: 'Email',
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: TextFormField(
-                            controller: _passwordController,
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                            ),
-                          ),
-                        ),
-                      ],
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                GradientScaffold(
+                  height: 230.0, 
+                  width: contextWidth(context),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 283.0,
+                    width: 300.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xffFFE9E9),
                     ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              AppButton(
-                  height: 63.0,
-                  width: 216.0,
-                  color: Colors.red,
-                  textdata: "Sign Up",
-                  fontSize: 23.0,
-                  fontWeight: FontWeight.bold,
-                  radius: 20.0,
-                  onTap: (){
-                     Navigator.pushNamed(context, LoginPage.route);
-                  },
-                ),
-                const SizedBox(height: 15.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Dont have an account? ",
-                      style: TextStyle(
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.bold
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: TextFormField(
+                                controller: _nameController,
+                                decoration: const InputDecoration(
+                                  labelText: 'Full Name',
+                                ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: TextFormField(
+                              controller: _emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: const InputDecoration(
+                                labelText: 'Email',
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: TextFormField(
+                              controller: _passwordController,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                labelText: 'Password',
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    InkWell(
-                      onTap:  ()=> Navigator.pushNamed(context, LoginPage.route),
-                      child: Text(
-                        "Login here",
+                  ),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                AppButton(
+                    height: 63.0,
+                    width: 216.0,
+                    color: Colors.red,
+                    textdata: "Sign Up",
+                    fontSize: 23.0,
+                    fontWeight: FontWeight.bold,
+                    radius: 20.0,
+                    onTap: (){
+                       Navigator.pushNamed(context, LoginPage.route);
+                    },
+                  ),
+                  const SizedBox(height: 15.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Dont have an account? ",
                         style: TextStyle(
                           fontSize: 14.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red
+                          fontWeight: FontWeight.bold
                         ),
                       ),
-                    ),
-                  ],
-                )
-            ],
+                      InkWell(
+                        onTap:  ()=> Navigator.pushNamed(context, LoginPage.route),
+                        child: Text(
+                          "Login here",
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+              ],
+            ),
           ),
         ),
       )
