40cf0a3c8b77115111a0c9c01efd8078f0982834
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-02-22 12:51:42 +0530

    Update main_activity.dart

--- lib/screens/main_activity.dart
+++ lib/screens/main_activity.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:social_clip/Provider/BottomNavProvider.dart';
+import 'package:social_clip/screens/BottomNavPages/Image/Image.dart';
+import 'package:social_clip/screens/BottomNavPages/Video/Video.dart';
 
-class MainActivity extends StatelessWidget {
+class MainActivity extends StatefulWidget {
   const MainActivity({super.key});
 
   @override
+  State<MainActivity> createState() => _MainActivityState();
+}
+
+class _MainActivityState extends State<MainActivity> {
+  List<Widget> pages=const [
+    ImageHomePage(),
+    VideoHomePage()
+  ];
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.image),label: "Image"),
-          BottomNavigationBarItem(icon: Icon(Icons.video_call),label: "Video")
-        ],
-      ),
+    return Consumer<BottomNavProvider>(
+      builder: (context,nav, child) {
+        return Scaffold(
+          body: pages[nav.currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: (value){
+              nav.ChangeIndex(value);
+            },
+            currentIndex: nav.currentIndex,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.image),label: "Image"),
+              BottomNavigationBarItem(icon: Icon(Icons.video_call),label: "Video")
+            ],
+          ),
+        );
+      }
     );
   }
 }
